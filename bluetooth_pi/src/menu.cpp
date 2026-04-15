c107063bdc689bc5b1c687b04161c477d2319b6f
Arcadi del Castillo <98622132+Arcadi-dCC@users.noreply.github.com>
2026-04-15 18:54:39 +0200

    enums for inputs and states

--- bluetooth_pi/src/menu.cpp
+++ bluetooth_pi/src/menu.cpp
@@ -2,40 +2,40 @@
 
 namespace menu{
 
-    volatile uint8 input = 0U; //Registers buttons input. Set by interrupt, Reset by stateMgr()
-    /*
-    * 0 -> No new input
-    * 1 -> Left button  & Short press
-    * 2 -> Left button  & Long press
-    * 3 -> Right button & Short press
-    * 4 -> Right button & Long press
-    */
+    enum Input{
+        NOTHING,
+        LEFT_SHORT,
+        LEFT_LONG,
+        LEFT_RELEASE,
+        RIGHT_SHORT,
+        RIGHT_LONG,
+        RIGHT_RELEASE
+    };
 
-    uint8 state = 0U; //Current state in the FSM register. In initial state by default
+    enum State{
+        AY,
+        BEY
+    }; //Current state in the FSM register. In initial state by default
 
-    /*
-    * 0b0xxxxxxx -> time menu
-    * 0b1xxxxxxx -> bluetooth menu
-    * 
-    * 
-    * 
-    * 
-    * 
-    * 
-    * 
-    *
-    */
+    volatile Input input;
+    State state;
 
     //Initializes menu FSM
     void config(void){
-        state = 0U;
+        state = AY;
         
     }
 
     //Manages the switching between states in the menu FSM
     void stateMgr(void){
 
-        input = 0U;
+        switch(state){
+            case AY:{
+                break;
+            }
+        }
+
+        input = NOTHING;
         //END:restore interrupt
     }
 
